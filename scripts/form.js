7c8889dd501304b58686f3a47e5fcbc9ca06e687
Imanuel Gittens <gittensimanuel@gmail.com>
2016-10-22 22:46:03 -0400

    Further enhanced the color select page and included logic for the review page.

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,7 @@
 
 var colorButton = document.querySelector('#colorChoices');
-console.log(colorButton);
+
+// console.log(colorButton);
 
 
 function addActiveClass() {
@@ -12,6 +13,8 @@ function addActiveClass() {
     if (item.nextElementSibling){
       item.nextElementSibling.classList.add('active');
     }else{
+      var formReview = document.querySelector('#review');
+      formReview.style.top = 0;
       console.log('Done');
     }
 
@@ -35,8 +38,38 @@ setBackgroundOptions();
 //Event listeners
 
 colorButton.addEventListener('click', function showColors(){
-  console.log('Inside function');
+  // console.log('Inside function');
   var choices = document.querySelector('#colorListing');
   choices.style.top = 0;
   choices.style.left = 0;
 });
+
+function handleColorClick(event){
+		event = event || window.event;
+		event.target = event.target || event.srcElement;
+		var element = event.target;
+
+    if(element.className === 'color-thumb') {
+      var result = element.getAttribute('data-value');
+      var selectVal = document.querySelector('#websiteColor');
+      selectVal.value = result;
+      var choices = document.querySelector('#colorListing');
+      choices.style.top = '-5000px';
+      choices.style.left = 0;
+      console.log(selectVal.value);
+    }
+
+		// var productNumber, value;
+    //
+		// if(element.className === 'apply-coupon'){
+		// 	value = document.getElementById('coupon').value;
+    //
+		// 	if(value){
+		// 		promoCode(value);
+		// 	}else{
+		// 		alert("Enter a coupon code.");
+		// 	}
+		// }
+	}
+
+  document.addEventListener('click', handleColorClick, true);
